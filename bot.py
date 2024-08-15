101e1692e3aef87d2657d82066d7516fab6698a3
BlackDrago0 <107365736+BlackDrago0@users.noreply.github.com>
2024-08-15 20:07:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,43 +1,47 @@
-
 import asyncio
+import logging
+import sys
+from os import getenv
 
-from aiogram import Router, Bot, Dispatcher
-from aiogram.client import bot
-from aiogram.types import Message, WebAppInfo, InlineKeyboardMarkup
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+
+TOKEN = '7321848780:AAE1ILkt2IwRnyXkQ9Xrl8LWRiujza8tHGY'
+
+dp = Dispatcher()
 
 
-def webapp_builder() -> InlineKeyboardMarkup:
-    builder = InlineKeyboardBuilder()
-    builder.button(
-        text="Let's Click!", web_app=WebAppInfo(
-            url=""
-        )
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Open",
+                    web_app=WebAppInfo(url="https://github.com/BlackDrago0/BlackDrago0.git"),
+                )
+            ]
+        ]
     )
-    return builder.as_markup()
+    await message.answer(f"Hi, welcome to pepetap", reply_markup=markup)
 
 
-router = Router()
 
 
-@router.message(CommandStart())
-async def start(message: Message) -> None:
-    await message.reply(
-        "Click! Click! Click",
-        reply_markup=webapp_builder()
-    )
+@dp.message(Command('privacy'))
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"pricavy link")
 
 
 async def main() -> None:
-    bot = Bot("", parse_mode=ParseMode.HTML)
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    dp = Dispatcher()
-    dp.include_router(router)
+    await dp.start_polling(bot)
 
-    await bot.delete_webhook(True)
-    await bot.start_polling(bot)
 
-    if __name__ == '__main__':
-        asyncio.run(main())
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
